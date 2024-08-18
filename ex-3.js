672bebebd3f1d745f93e1fc8de7e8fcd06edf80a
Athirach <wathirach@gmail.com>
2024-08-18 14:45:49 +0700

    feat:Example primitive-values bp5-Wan

--- ex-3.js
+++ ex-3.js
@@ -1,6 +1,6 @@
 // Exercise #3: Uploading Progress
-
 let totalFileSize = 250000;
 let uploadedSize = 30000;
 
 // Start coding here
+let uploadProgress = (uploadedSize / totalFileSize)*100;
--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,6 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let totalPriceProduct = 27000;
+let discount = totalPriceProduct * 0.25;
+let totalAmount = totalPriceProduct - discount;
